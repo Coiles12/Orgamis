8a754d47a61644d12c59abc7cfa8190fe11ea515
Coiles12 <lecointregustave@gmail.com>
2026-07-24 22:19:35 +0200

    aaaa

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -4,8 +4,8 @@ import { env } from "@/lib/env";
 
 export function createAdminClient() {
   return createClient(
-    env.NEXT_PUBLIC_SUPABASE_URL,
-    env.SUPABASE_SERVICE_ROLE_KEY,
+    env.NEXT_PUBLIC_SUPABASE_URL as string,
+    env.SUPABASE_SERVICE_ROLE_KEY as string,
     {
       auth: {
         autoRefreshToken: false,
