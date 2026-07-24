4c18dc96c11266fd32cbc02bc49795533e4965f4
Coiles12 <lecointregustave@gmail.com>
2026-07-24 22:05:51 +0200

    Cestbon?

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Ceci autorise Vercel à déployer même s'il y a des avertissements ESLint
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
